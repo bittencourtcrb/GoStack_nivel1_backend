e43b88ee485568212ad80f1be5594fff6173eb6f
unknown <bittencourtcrb@gmail.com>
2020-08-18 23:26:38 -0300

    add CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { uuid, isUuid } = require('uuidv4');
 const { response } = require('express');
+const cors = require('cors')
 
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json())
 
